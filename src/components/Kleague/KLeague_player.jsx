ccfdbcb2cb50d1f02398aba9505d190e438a5e36
minjo1234 <whals777530@naver.com>
2023-10-20 10:04:27 +0900

    10:04 KLeague_player_detail추가

--- src/components/Kleague/KLeague_player.jsx
+++ src/components/Kleague/KLeague_player.jsx
@@ -1,16 +1,16 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-export default function KLeague_player() {
-  const [playerdata, setPlayerData] = useState([]);
+export default function KLeaguePlayer() {
+  const [playerData, setPlayerData] = useState([]);
   const [playerDetail, setPlayerDetail] = useState(null);
+  const [detailFetched, setDetailFetched] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get("/kLeague/player");
         setPlayerData(response.data);
-        console.log(response.data);
       } catch (err) {
         console.error(err);
       }
@@ -18,22 +18,22 @@ export default function KLeague_player() {
     fetchData();
   }, []);
 
-  useEffect(() => {
-    const fetchPlayerDetail = async (k_league_player_id) => {
-      try {
-        const response = await axios.get(
-          `/kLeague/player/${k_league_player_id}`
-        );
-        setPlayerDetail(response.data);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    if (playerDetail) {
-      fetchPlayerDetail(playerDetail.k_league_player_id);
+  const fetchPlayerDetail = async (k_league_player_id) => {
+    try {
+      const response = await axios.get(`/kLeague/player/${k_league_player_id}`);
+      setPlayerDetail(response.data);
+      setPlayerDetail(response.data);
+      console.log(response.data);
+      setDetailFetched(true);
+    } catch (err) {
+      console.error(err);
     }
-  }, [playerDetail]);
+  };
+
+  const clearPlayerDetail = () => {
+    setPlayerDetail(null);
+    setDetailFetched(false);
+  };
 
   const commonStyle = {
     display: "flex",
@@ -48,30 +48,34 @@ export default function KLeague_player() {
     verticalAlign: "baseline",
     borderCollapse: "collapse",
     borderSpacing: 0,
-    width: "80%",
+    width: "100%",
     borderTop: "1px solid #929292",
-    justifyContent: "space-between", // Add this line
+    justifyContent: "space-between",
     background: "#fff",
   };
 
-  const listItemStyle = {
-    display: "flex", // 가로로 배열
-    margin: "5px", // 아이템 간격
-  };
-
   const pStyle = {
-    padding: "10px", // Adjust the padding as needed
-    lineHeight: "1.5", // Adjust the line height as needed
+    padding: "10px",
+    lineHeight: "1.5",
   };
 
-  const pStyleElement = {
-    padding: "21px", // Adjust the padding as needed
-    lineHeight: "1.5", // Adjust the line height as needed
+  const playerDetailList = {
+    fontSize: "80px",
+    border: "1px solid #ccc",
+    padding: "10px",
+    margin: "10px",
+    background: "#fff",
   };
 
   const showPlayerDetail = (data) => {
-    setPlayerData(data);
-    fetchPlayerDetail(data.k_league_player_id);
+    if (
+      !detailFetched ||
+      playerDetail.k_league_player_id !== data.k_league_player_id
+    ) {
+      fetchPlayerDetail(data.k_league_player_id);
+    } else {
+      clearPlayerDetail();
+    }
   };
 
   return (
@@ -81,30 +85,43 @@ export default function KLeague_player() {
         <p style={pStyle}>팀 이름</p>
         <p style={pStyle}>번호</p>
         <p style={pStyle}>사진</p>
+        <p style={pStyle}>디테일</p>
       </div>
-      <div>
-        {playerdata.map((data, index) => (
-          <div
-            key={index}
-            style={{ ...commonStyle, marginTop: "10px" }}
-            onClick={() => showPlayerDetail(data)}
-          >
-            <p>{data.k_league_player_id}</p>
-            <p>{data.k_league_player_team}</p>
-            <p>{data.k_league_player_name}</p>
-            <p>{data.k_league_player_num}</p>
-            <p>
-              <img
-                src={data.k_league_player_img_url}
-                width="200px"
-                height="200px"
-              />
-            </p>
-          </div>
-        ))}
+      <div style={{ display: "flex" }}>
+        <div style={{ width: "70%" }}>
+          {playerData.map((data, index) => (
+            <div
+              key={index}
+              style={{ ...commonStyle, marginTop: "10px" }}
+              onClick={() => showPlayerDetail(data)}
+            >
+              <p>{data.k_league_player_id}</p>
+              <p>{data.k_league_player_team}</p>
+              <p>{data.k_league_player_name}</p>
+              <p>{data.k_league_player_num}</p>
+              <p>
+                <img
+                  src={data.k_league_player_img_url}
+                  width="200px"
+                  height="200px"
+                  alt={data.k_league_player_name}
+                />
+              </p>
+            </div>
+          ))}
+        </div>
         {playerDetail && (
-          <div>
-            <p>detail정보 확인완료 </p>
+          <div style={{ width: "20%" }}>
+            <div style={playerDetailList}>
+              <p>{playerDetail.player_name}</p>
+              <p>{playerDetail.player_team}</p>
+              <p>{playerDetail.player_number}</p>
+              <p>{playerDetail.player_position}</p>
+              <p>{playerDetail.player_nationality}</p>
+              <p>{playerDetail.player_height}</p>
+              <p>{playerDetail.player_weight}</p>
+              <p>{playerDetail.player_birthdate}</p>
+            </div>
           </div>
         )}
       </div>
