7c51fc3954b6ee1da3831def37645ab99144f42d
Jo Min <122578400+minjo1234@users.noreply.github.com>
2023-10-20 10:38:24 +0900

    Choi (#7)
    * STyle 추가전
    
    * 17:24
    
    * 10:04 KLeague_player_detail추가
    
    * 10:06
    
    * 10:19 currentkbo-match연동동
    
    * 10:27

--- src/pages/match/index.js
+++ src/pages/match/index.js
@@ -1,13 +1,13 @@
 import MainLayout from "../layout/MainLayout";
 import MatchList from "../../components/MatchList";
+import CurrentKbo from "../../components/CurrentGame/CurrentKbo";
 
-
-function Match(){
-
-    return(
-        <MainLayout>
-        </MainLayout>
-    )
+function Match() {
+  return (
+    <MainLayout>
+      <CurrentKbo />
+    </MainLayout>
+  );
 }
 
 export default Match;
