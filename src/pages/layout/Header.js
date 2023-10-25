8c2a40de57ce1288881cbd76bfc40c2e01c146f0
Jo Min <122578400+minjo1234@users.noreply.github.com>
2023-10-25 10:57:06 +0900

    09:24 (#18)
    Co-authored-by: OhEunGuel <oek1228@gmail.com>

--- src/pages/layout/Header.js
+++ src/pages/layout/Header.js
@@ -3,18 +3,102 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { fetchUser } from '../../api/UserAPI';
 
-const HeaderContainer = styled.div`
+const Header = () => {
+  const [user, setUser] = useState({});
+  const ACCESS_TOKEN = localStorage.getItem('accessToken');
+
+  useEffect(() => {
+    if (ACCESS_TOKEN) {
+      fetchUser()
+        .then((response) => {
+          setUser(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, [ACCESS_TOKEN]);
+
+  const handleLogout = () => {
+    localStorage.clear();
+    window.location.href = '/';
+  };
+
+  const menuItems = [
+    { label: 'Home', to: '/' },
+    { label: 'Community', to: '/Community' },
+    { label: 'Match', to: '/Match' },
+    { label: 'News', to: '/News' },
+    { label: 'Team', to: '/Team' }
+  ];
+
+  return (
+    <HeaderContainer>
+      <div className="header">
+        {menuItems.map((item, index) => (
+          <MenuItem key={index} to={item.to}>
+            {item.label}
+          </MenuItem>
+        ))}
+      </div>
+      {ACCESS_TOKEN ? (
+        <Dropdown>
+          <Dropth>{user.nickName}</Dropth>
+          <DropdownContent>
+            <DropdownContentLink to="/my-page">My Page</DropdownContentLink>
+            <DropdownContentLink to="/" onClick={handleLogout}>
+              Logout
+            </DropdownContentLink>
+          </DropdownContent>
+        </Dropdown>
+      ) : (
+        <Dropdown>
+          <Dropth>Log In</Dropth>
+          <DropdownContent>
+            <DropdownContentLink to="/login">Log In</DropdownContentLink>
+            <DropdownContentLink to="/join">Sign Up</DropdownContentLink>
+          </DropdownContent>
+        </Dropdown>
+      )}
+    </HeaderContainer>
+  );
+};
+
+const HeaderContainer = styled.header`
+  position: fixed;
   background-color: #333333;
   color: white;
   padding: 10px 20px;
   display: flex;
   justify-content: space-between;
   align-items: center;
+  width: 100%;
+`;
+
+const MenuItem = styled(Link)`
+  border-radius: 4px;
+  background-color: #333333;
+  font-weight: 400;
+  color: #f8f3f3;
+  padding: 12px;
+  width: 100px;
+  text-align: center;
+  cursor: pointer;
+  font-size: 20px;
+  margin-right: 20px;
+  text-decoration: none;
+
+  &:last-child {
+    margin-right: 0;
+  }
+
+  &:hover {
+    background-color: #555555;
+  }
 `;
 
 const Dropdown = styled.div`
   position: relative;
-  margin-right: 100px;
   display: inline-block;
 `;
 
@@ -53,179 +137,4 @@ const DropdownContentLink = styled(Link)`
   }
 `;
 
-const HomeLink = styled(Link)`
-  border-radius: 4px;
-  background-color: #333333;
-  font-weight: 400;
-  color: #f8f3f3;
-  padding: 12px;
-  width: 100px;
-  text-align: left;
-  cursor: pointer;
-  font-size: 20px;
-  margin-right: auto;
-`;
-
-const CommunityLink = styled(Link)`
-  border-radius: 4px;
-  background-color: #333333;
-  font-weight: 400;
-  color: #f8f3f3;
-  padding: 12px;
-  width: 100px;
-  text-align: center;
-  cursor: pointer;
-  font-size: 20px;
-`;
-
-const MatchLink = styled(Link)`
-  border-radius: 4px;
-  background-color: #333333;
-  font-weight: 400;
-  color: #f8f3f3;
-  padding: 12px;
-  width: 100px;
-  text-align: center;
-  cursor: pointer;
-  font-size: 20px;
-`;
-
-const NewsLink = styled(Link)`
-  border-radius: 4px;
-  background-color: #333333;
-  font-weight: 400;
-  color: #f8f3f3;
-  padding: 12px;
-  width: 100px;
-  text-align: center;
-  cursor: pointer;
-  font-size: 20px;
-`;
-
-const TeamInfoLink = styled(Link)`
-  border-radius: 4px;
-  background-color: #333333;
-  font-weight: 400;
-  color: #f8f3f3;
-  padding: 12px;
-  width: 100px;
-  text-align: left;
-  cursor: pointer;
-  font-size: 20px;
-`;
-
-const Header = () => {
-  const [user, setUser] = useState({});
-  const ACCESS_TOKEN = localStorage.getItem('accessToken');
-
-  useEffect(() => {
-    if (ACCESS_TOKEN) {
-      fetchUser()
-        .then((response) => {
-          console.log(response);
-          setUser(response);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    }
-  }, [ACCESS_TOKEN]);
-
-  const handleLogout = async () => {
-    localStorage.removeItem('accessToken');
-    localStorage.removeItem('refreshToken');
-    localStorage.removeItem('tokenType');
-    window.location.href = '/';
-  };
-
-  const dynamicContent = ACCESS_TOKEN ? (
-    <Dropdown>
-      <Dropth>{user.nickName}</Dropth>
-      <DropdownContent>
-        <DropdownContentLink to="/my-page">My Page</DropdownContentLink>
-        <DropdownContentLink to="/" onClick={handleLogout}>
-          Logout
-        </DropdownContentLink>
-      </DropdownContent>
-    </Dropdown>
-  ) : (
-    <Dropdown>
-      <Dropth>Log In</Dropth>
-      <DropdownContent>
-        <DropdownContentLink to="/login">Log In</DropdownContentLink>
-        <DropdownContentLink to="/join">Sign Up</DropdownContentLink>
-      </DropdownContent>
-    </Dropdown>
-  );
-
-  return (
-    <HeaderContainer>
-      <div className="header">
-        <HomeLink to="/">Home</HomeLink>
-        <CommunityLink to="/Community">Community</CommunityLink>
-        <MatchLink to="/Match">Match</MatchLink>
-        <NewsLink to="/News">News</NewsLink>
-        <TeamInfoLink to="/TeamInfo">TeamInfo</TeamInfoLink>
-      </div>
-      {dynamicContent}
-    </HeaderContainer>
-  );
-};
 export default Header;
-
-export const Dropdown = styled.div`
-  position: relative;
-  margin-right: 100px;
-  display: inline-block;
-`
-export const Dropth = styled.button`
-  border : 1px solid #202025;
-  border-radius : 4px;
-  background-color: #202025;
-  font-weight: 400;
-  color : #ffff;
-  padding : 12px;
-  width :100px;
-  text-align: left;
-  cursor : pointer;
-  font-size : 20px;
-`
-
-export const DropdownContent = styled.div`
-  display : none;
-  position : absolute;
-  z-index : 1; /*다른 요소들보다 앞에 배치*/
-  font-weight: 400;
-  background-color: #f9f9f9;
-  min-width : 100px;
-  ${Dropdown}:hover &{
-    display:block;
-  }
-`
-
-export const DropdownContentLink = styled(Link)`
-  display : block;
-  text-decoration : none;
-  color : rgb(37, 37, 37);
-  font-size: 12px;
-  padding : 12px 20px;
-  &:hover{
-    background-color : #ececec
-  }
-`
-export const HomeLink = styled(Link)`
-  border : 1px solid #202025;
-  border-radius : 4px;
-  background-color: #202025;
-  font-weight: 400;
-  color : #F8F3F3;
-  padding : 12px;
-  width :100px;
-  text-align: left;
-  cursor : pointer;
-  font-size : 20px;
-`
-const HomeLi = styled.li`
-  
-`
-
